15044dbf1fe66cadfda6c13355954c1c842e448c
Hernanda Khoiriyah Putri <104390322+hernandakp@users.noreply.github.com>
2022-07-06 01:16:25 +0700

    Update Determinant Matrices 2x2 - Array.py

--- Determinant Matrices 2x2 - Array.py
+++ Determinant Matrices 2x2 - Array.py
@@ -1,14 +1,14 @@
 'Author: hernanda472@gmail.com'
 
-#Matriks ukuran 2*2 dengan NumPy
+# Matriks ukuran 2*2 dengan NumPy
+
 import numpy as np
+
 print("Matriks P ukuran 2*2")
 P = np.array([(1,2),(3,4)])
-
 print(P)
 
 # Determinan Matriks
 print("Determinan Matriks P ukuran 2*2")
 det_P = np.linalg.det(P)
-
 print(det_P)
